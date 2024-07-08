570b5e755c693238a91103e22b0939825f13effc
Rubylenshy <reztomoloju@gmail.com>
2024-07-08 16:55:32 +0100

    initialize

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app-portfolio">
+        <HomeView />
+    </div>
+</template>
+
+<script>
+import HomeView from "./pages/home.vue";
+
+export default {
+    name: "App",
+    components: {
+        HomeView,
+    },
+};
+</script>
---
+++ src/pages/home.vue
@@ -0,0 +1,23 @@
+<script>
+    import NavBar from "../components/navbar.vue";
+    import FooterComp from "../components/footer.vue";
+
+    export default {
+        name: 'HomeView',
+        components: {
+            NavBar,
+            FooterComp
+        },
+        props: {
+            
+        }
+    }
+</script>
+
+<template>
+    <div class="">
+        <nav-bar />
+
+        <footer-comp />
+    </div>
+</template>
