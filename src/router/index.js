0ffcdd160b0d22c333ef7273ce726cb18b0df277
Rubylenshy <reztomoloju@gmail.com>
2024-08-08 23:18:01 +0100

    add router js file

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import HomeView from '../pages/home.vue';
+import ProjectPage from '../pages/project-page.vue';
+import AboutPage from '../pages/about.vue';
+import ContactPage from '../pages/contact.vue';
+
+const routes = [
+	{ path: '/', name: 'Home', component: HomeView },
+    { path: '/about', name: 'About', component: AboutPage },
+    { path: '/projects-page', name: 'Projects', component: ProjectPage },
+    { path: '/contact', name: 'Contact', component: ContactPage },
+];
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+});
+
+export default router;
