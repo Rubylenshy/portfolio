9ff2f267aff7f52cff42fa58ea9f6034cc59434d
Rubylenshy <reztomoloju@gmail.com>
2024-12-13 05:52:26 +0100

    refactor: update social icons

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -4,10 +4,8 @@
             <div class="main-container d-flex flex-column flex-md-row align-items-center justify-content-between gap-3">
                 <div class="scroll-to-top d-flex d-md-none "><img src="../assets/svg/up-arrow.svg" alt="" @click="() => scrollToTop()"></div>
                 
-                <!-- If you give me some credit or shoutout here by linking to my website, then it will be a big thing for me and will help me a lot :) -->
                 <div class="text-center text-md-left">
                     &copy; 2023-2024
-                    <!-- <cite><br> Inspired by <a rel="noreferrer" target="_blank" href="https://rammaheshwari.com">Ram Maheshwari</a></cite> -->
                 </div>
                 <ul class="footer-links d-flex gap-4 justify-content-center">
                     <li v-for="link in socialLinks" :key="link">
@@ -26,9 +24,8 @@
             return {
                 socialLinks: [
                     { name: 'Twitter', icon: 'fa-brands fa-x-twitter', href: 'https://twitter.com/tomoloj_' },
-                    { name: 'Daily dev', icon: 'fa-brands fa-dev', href: 'https://dev.to/rubylenshy' },
+                    { name: 'Instagram', icon: 'fa-brands fa-instagram', href: 'https://www.instagram.com/reuben.ig_' },
                     { name: 'GitHub', icon: 'fa-brands fa-github', href: 'https://github.com/Rubylenshy' },
-                    { name: 'Slack', icon: 'fa-brands fa-slack', href: 'https://slack.com' },
                     { name: 'LinkedIn', icon: 'fa-brands fa-linkedin', href: 'https://www.linkedin.com/in/reuben-tomoloju/' },
                     { name: 'Email', icon: 'fa-solid fa-envelope', href: 'mailto:reztomoloju@gmail.com' }
                 ],
