3f9ec227177a0b3da7c495258fa1d949f00f53f6
Rubylenshy <reztomoloju@gmail.com>
2025-01-11 20:16:37 +0100

    feat: add rounter meta title

--- src/router/index.js
+++ src/router/index.js
@@ -6,15 +6,23 @@ import AboutPage from '../pages/about.vue';
 import ContactPage from '../pages/contact.vue';
 
 const routes = [
-	{ path: '/', name: 'Home', component: HomeView },
-    { path: '/about', name: 'About', component: AboutPage },
-    { path: '/projects', name: 'Projects', component: ProjectPage },
-    { path: '/contact', name: 'Contact', component: ContactPage },
+	{ path: '/', name: 'Home', component: HomeView, meta: { title: 'Reuben Oluwafemi | Home' }},
+    { path: '/about', name: 'About', component: AboutPage, meta: { title: 'Reuben Oluwafemi | About' }},
+    { path: '/projects', name: 'Projects', component: ProjectPage, meta: { title: 'Reuben Oluwafemi | Projects' }},
+    { path: '/contact', name: 'Contact', component: ContactPage, meta: { title: 'Reuben Oluwafemi | Contact' }},
 ];
 
 const router = createRouter({
 	history: createWebHistory(),
 	routes,
+});
+
+router.beforeEach((to, from, next) => {
+    // Access the meta title property and set it as the document title
+    if (to.meta.title) {
+      document.title = to.meta.title;
+    }
+    next();
 });
 
 export default router;
